88fc51dd21b97e2b134abe435a1beb6bd57c2a76
Sebastian Bassi <sebastian@toyoko.io>
2020-09-16 03:33:01 -0700

    adding working localhost based on bootstrap

--- moonrock/sites/localhost/index.py
+++ moonrock/sites/localhost/index.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, url_for, render_template
 app = Flask(__name__)
+
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    print(url_for('static', filename='style.css'))
+    return render_template('indexblog.html')
+
+@app.route('/carousel')
+def carousel():
+    return render_template('carousel.html')
+
+
+@app.route('/checkout')
+def checkout():
+    return render_template('checkout.html')
