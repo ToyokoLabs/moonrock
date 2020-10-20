890176b501d7354c1a35d4873420b0d07eaa46a3
Virginia González <vgonzalez@gmail.com>
2020-10-20 12:42:50 -0700

    website
    website changes

--- moonrock/sites/localhost/index.py
+++ moonrock/sites/localhost/index.py
@@ -8,11 +8,23 @@ def hello_world():
     print(url_for('static', filename='style.css'))
     return render_template('indexblog.html')
 
+@app.route('/indexblog')
+def indexblog():
+    return render_template('indexblog.html')
+
+
 @app.route('/carousel')
 def carousel():
     return render_template('carousel.html')
 
-
 @app.route('/checkout')
 def checkout():
     return render_template('checkout.html')
+
+@app.route('/album')
+def album():
+    return render_template('album.html')
+
+@app.route('/pricing')
+def pricing():
+    return render_template('pricing.html')
